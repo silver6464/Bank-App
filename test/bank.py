66d901f4c620c1d0df2fd766a37fb8a5c0449e22
Silver <silverserate64@gmail.com>
2014-11-18 19:38:37 -0800

    Added my work
    added all of the files needed.

---
+++ test/bank.py
@@ -0,0 +1,11 @@
+class Bank(object):
+    def __init__(self):
+        self.accounts = {}
+
+    def add_account(self, account):
+        self.accounts[account.account_number] = account.balance
+    def get_account_balance(self, account_number):
+        return self.accounts.get(account_number)
+    def account_withdraw(self, account,amount):    	
+    	self.accounts[account.account_number] =- 20
+    	
---
+++ test/bank_test.py
@@ -0,0 +1,29 @@
+import unittest
+from account import Account
+from bank import Bank
+
+class BankTest(unittest.TestCase):
+    def test_bank_is_initially_empty(self):
+        bank = Bank()
+        self.assertEqual({}, bank.accounts)
+        self.assertEqual(len(bank.accounts), 0)
+    def test_add_account(self):
+        bank = Bank()
+        account_1 = Account(001, 50)
+        account_2 = Account(002, 100)
+        bank.add_account(account_1)
+        bank.add_account(account_2)
+        self.assertEqual(len(bank.accounts), 2)
+    def test_get_account_balance(self):
+        bank = Bank()
+        account_1 = Account(001, 50)
+        bank.add_account(account_1)
+        self.assertEqual(bank.get_account_balance(001), 50)
+    def test_account_withdraw(self):
+        bank = Bank()
+        account_1 = Account(001, 50)
+        bank.account_withdraw(account_1,amount)
+        self.assertEqual(bank.get_account_balance(001), 20)
+
+if __name__ == '__main__':
+    unittest.main()
