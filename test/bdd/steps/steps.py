1e915b1fe05666d34d8beee5a472b0a9d1fe8198
silver6464 <silverserate64@gmail.com>
2014-11-25 16:47:51 +0800

    Updated to CH6 files including BDD,features,steps,unit, etc.

---
+++ test/bdd/steps/steps.py
@@ -0,0 +1,29 @@
+﻿from lettuce import *
+from nose.tools import assert_equal,assert_in
+from webtest import TestApp
+from bank_app import app,BANK
+from bank.account import Account
+
+@step(u'I create account "([^"]*)" with balance of "([^"]*)"')
+def i_create_account_with_balance_of_group1(step, account_number, balance):
+	a = Account(account_number, balance)
+	BANK.add_account(a)
+
+@step(u'I see a balance of "([^"]*)"')
+def then_i_see_a_balance_of_group1(step, expected_balance):
+	assert_in ("Balance: {}".format(expected_balance),
+	world.form_response.text)
+
+@step(u'I enter the account number "([^"]*)"')
+def when_i_enter_the_account_number_group1(step, account_number):
+	form = world.response.forms['account-form']
+	form['account_number'] = account_number
+	world.form_response = form.submit()
+	assert_equal(world.form_response.status_code, 200)
+	
+@step(u'I visit the homepage')
+def i_visit_the_homepage(step):
+	world.browser = TestApp(app)
+	world.response = world.browser.get('http://localhost:5000/')
+	assert_equal(world.response.status_code, 200)
+	#(world.response.text, u'Hello World!')
---
+++ test/unit/lettuce_test.py
@@ -0,0 +1,8 @@
+﻿from lettuce import *
+from account import Account
+from bank import Bank
+@step(u'account number 0001 is a valid account')
+def given_account_number_0001_is_a_valid_account(step):
+	account = Account(0001, 50)
+	bank = Bank()
+	bank.add_account(account)
