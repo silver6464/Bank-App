ed14a5ac2995a075fc7134e77ebbd2e7b719b10b
silver6464 <silverserate64@gmail.com>
2014-12-02 17:38:09 +0800

    CH8 cProfile1

--- bank_app (copy).py
+++ bank_app (copy).py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, request
-
-from bank.account import Account
 from bank.bank import Bank
 app = Flask(__name__)
 BANK = Bank()
@@ -9,13 +7,7 @@ def hello_world():
 	account_number = request.args.get('account_number')
 	balance = BANK.get_account_balance(account_number)
 	return render_template('index.html', balance=balance)
+	#return render_template ('index.html')
 
 if __name__ == '__main__':
-	import cProfile
-
-	account = Account('1111',50)
-	BANK.add_account(account)
-	cProfile.run('app.run(debug=True)')
-	#cProfile.run('app.run(debug=True)', sort='time')
-	
-
+	app.run(debug=True)
